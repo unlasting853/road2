d303499fd2df89c5d767e67fa71e9526fa43a2ac
unlasting853 <1124168237@qq.com>
2023-05-09 18:12:43 +0800

    初始化

---
+++ src/main/java/com/road2/road2/Road2Application.java
@@ -0,0 +1,13 @@
+package com.road2.road2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Road2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Road2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/road2/road2/Road2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.road2.road2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Road2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
